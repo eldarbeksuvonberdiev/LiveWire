b38b6762025732feed227980e7b3efda2241481b
eldarbeksuvonberdiev <eldorbek.suvonberdiev@mail.ru>
2024-11-27 19:29:06 +0500

    LiveWire da ozgina o'zgarishlar qilindi

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\CategoryController;
 use App\Livewire\CalcComponent;
 use App\Livewire\HomeComponent;
 use App\Livewire\StudentComponent;
@@ -13,3 +15,5 @@ Route::get('/', function () {
 Route::get('home',HomeComponent::class);
 Route::get('student',StudentComponent::class);
 Route::get('calc',CalcComponent::class);
+Route::get('category',[CategoryController::class,'index']);
+Route::get('car',[CarController::class,'index']);
