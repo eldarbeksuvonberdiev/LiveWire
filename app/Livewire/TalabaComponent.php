060564a7d3e5082508e6905f98dcaf1eb46006e1
eldarbeksuvonberdiev <eldorbek.suvonberdiev@mail.ru>
2024-12-04 16:36:02 +0500

    Davomat qilishda taxminan hammasi tugadi

--- app/Livewire/TalabaComponent.php
+++ app/Livewire/TalabaComponent.php
@@ -8,9 +8,9 @@ use Livewire\Component;
 
 class TalabaComponent extends Component
 {
-    public $date, $students;
+    public $date, $students,$talabaId,$attendanceDate;
 
-    public function mount() 
+    public function mount()
     {
         $this->date = Carbon::now();
         $this->students = Talaba::all();
@@ -20,10 +20,10 @@ class TalabaComponent extends Component
     {
         $daysInMonth = $this->date->daysInMonth;
         $days = [];
-        for ($i=1; $i <= $daysInMonth; $i++) { 
-            $days[] = Carbon::create($this->date->year,$this->date->month,$i);
+        for ($i = 1; $i <= $daysInMonth; $i++) {
+            $days[] = Carbon::create($this->date->year, $this->date->month, $i);
         }
-        return view('livewire.talaba-component',['days' =>$days]);
+        return view('livewire.talaba-component', ['days' => $days]);
     }
 
     public function changeDate($date)
@@ -31,4 +31,18 @@ class TalabaComponent extends Component
         $this->date = Carbon::parse($date);
     }
 
+    public function inputView($id,$date) 
+    {
+        $this->talabaId = $id;
+        $this->attendanceDate = $date;
+    }
+
+    public function createAttendance(Talaba $talaba,$date,$value)
+    {
+        $talaba->attendances()->updateOrCreate([
+            'date' => Carbon::parse($date),
+            'value' => $value
+        ]);
+        $this->reset(['talabaId','attendanceDate']);
+    }
 }
--- app/Models/Talaba.php
+++ app/Models/Talaba.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Talaba extends Model
@@ -11,5 +12,10 @@ class Talaba extends Model
     public function attendaces() 
     {
         return $this->hasMany(Attendance::class,'talaba_id');
+    }
+
+    public function checks($date)
+    {
+        return $this->attendaces()->where('date',Carbon::parse($date))->first();
     }
 }
